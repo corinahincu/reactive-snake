245ce4761a56882eaa118250f312f89026d4feda
corinahincu <hincucorina97@gmail.com>
2023-11-05 14:22:52 +0200

    some changes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { SnakeHead } from "./snake/ui";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <SnakeHead />
+    <SnakeHead dir="down"/>
   </React.StrictMode>
 );
 
--- src/snake/ui.js
+++ src/snake/ui.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+/* import { useState, useEffect } from "react"; */
 
 import "./snake.scss";
 
@@ -9,14 +9,37 @@ import "./snake.scss";
 
 
 
-const SnakeHead = () => {
-  const [dir, setDir] = useState("up");
+const SnakeHead = ({ dir = "up" }) => {
+
+  return <div className={`snake__head dir-${dir}`}></div>;
+};
+
+
+// varianta in care foloses useState / useEffect
+/* const SnakeHead = ({dir = "up"}) => {
+
+  const [dir, setDir] = useState(dir)
+
+  useEffect(() => {
+    setTimeout(()=> {
+      console.log("change direction")
+      setDir("right")
+    },5000)
+  },[])
+
+  return <div className={`snake__head dir-${dir}`}></div>;
+} */
+
+// varianta initiala
+/*   const [dir, setDir] = useState("up");
 
   setTimeout(() => {
     console.log("Changed direction");
-    setDir("down");
+    setDir("left");
   }, 5000);
-  return <div className={`snake__head dir-${dir}`}>snake</div>;
-};
+  return <div className={`snake__head dir-${dir}`}></div>;
+}; */
+
+
 
 export { SnakeHead };
