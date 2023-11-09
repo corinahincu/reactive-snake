3c9f0e961cca2591dccffc94f7e3011f1b294fe2
corinahincu <hincucorina97@gmail.com>
2023-11-09 20:56:42 +0200

    lesson 2

--- src/snake/ui.js
+++ src/snake/ui.js
@@ -1,9 +1,6 @@
 import { useState } from "react";
 import "./snake.scss";
 
-// hw2: using props.dir (with destrucuring & default value of 'up') make it so the snakehead renders in either of the next 2 situations:
-// <SneakeHead/> or <SnakeHead dir="right" />
-
 const SnakeHead = ({ dir = "up", top=0, left=0 }) => {
   const [_dir, setDir] = useState(dir)
   const [_top, setTop] = useState(top)
